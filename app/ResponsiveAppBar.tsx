47037882fffbaf38d6198a7251038b0da923f77d
John Marshall <34883+swax@users.noreply.github.com>
2023-09-07 21:43:06 -0700

    Mocked up sketch page

---
+++ app/ResponsiveAppBar.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { MouseEvent, useState } from "react";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+
+const pages = ["Shows", "Sketches", "Themes", "Actors"];
+const settings = ["Profile", "Account", "Logout"];
+
+function ResponsiveAppBar() {
+  // Hooks
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  // Event handlers
+  function handleOpenNavMenu(event: MouseEvent<HTMLElement>) {
+    setAnchorElNav(event.currentTarget);
+  }
+
+  function handleOpenUserMenu(event: MouseEvent<HTMLElement>) {
+    setAnchorElUser(event.currentTarget);
+  }
+
+  function handleCloseNavMenu() {
+    setAnchorElNav(null);
+  }
+
+  function handleCloseUserMenu() {
+    setAnchorElUser(null);
+  }
+
+  // Rendering
+  const appName = "SketchTV.lol";
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <LiveTvIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "cursive",
+              fontWeight: 700,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {appName}
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <LiveTvIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {appName}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Skip to another sketch">
+              <IconButton onClick={handleOpenUserMenu} sx={{ mr: 1 }} size="large">
+                <SkipNextIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="John Marshall" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,261 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+import ForumIcon from "@mui/icons-material/Forum";
+import GroupsIcon from "@mui/icons-material/Groups";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import NotesIcon from "@mui/icons-material/Notes";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Backdrop,
+  Box,
+  Button,
+  Chip,
+  Container,
+  Paper,
+  Rating,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import { useState } from "react";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import SketchData from "./SketchData";
+import style from "./page.module.css";
 
 export default function Home() {
+  // Hooks
+  const [playerOpen, setPlayerOpen] = useState(false);
+
+  // Event handlers
+  function handleOpenPlayer() {
+    setPlayerOpen(true);
+  }
+
+  function handleClosePlayer() {
+    setPlayerOpen(false);
+  }
+
+  // Rendering
+  const imgWidth = 400;
+  const imgHeight = imgWidth * (9 / 16);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <ResponsiveAppBar />
+      <Container sx={{ mt: 1, pb: 4 }}>
+        <Typography variant="h4">{SketchData.title}</Typography>
+        <Typography variant="subtitle1">
+          {SketchData.show.title} ({SketchData.season.year})
+        </Typography>
+        <Box>
+          <div
+            onClick={handleOpenPlayer}
+            style={{
+              borderRadius: 8,
+              cursor: "pointer",
+              overflow: "hidden",
+              position: "relative",
+              width: imgWidth,
+              height: imgHeight,
+            }}
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              alt={SketchData.title}
+              fill
+              objectFit="cover"
+              src={SketchData.preview_img.url}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          </div>
+        </Box>
+        <Box marginTop={1}>
+          <Typography variant="body1">{SketchData.teaser}</Typography>
+        </Box>
+        <Box marginTop={1}>
+          <Stack direction="row" spacing={1}>
+            {SketchData.links.map((link, i) => (
+              <Chip
+                clickable
+                component="a"
+                href={link.url}
+                key={i}
+                label={link.text}
+                size="small"
+                target="_blank"
+                variant="outlined"
+              />
+            ))}
+          </Stack>
+        </Box>
+        <Box marginTop={2}>
+          <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+        </Box>
+        <Box sx={{ marginTop: 2 }}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel0a-content"
+              id="panel0a-header"
+            >
+              <LocalOfferIcon />
+              <Typography fontWeight="bold" marginLeft={1}>
+                Tags
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack direction="row" spacing={1}>
+                {SketchData.tags.map((tag, i) => (
+                  <Chip
+                    clickable
+                    key={i}
+                    label={
+                      <span>
+                        {tag.category}&nbsp;/&nbsp;{tag.name}
+                      </span>
+                    }
+                    size="small"
+                    variant="outlined"
+                  />
+                ))}
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <GroupsIcon />
+              <Typography fontWeight="bold" marginLeft={1}>
+                Characters
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <TableContainer>
+                <Table aria-label="simple table">
+                  <TableBody>
+                    {SketchData.characters.map((character, i) => (
+                      <TableRow key={i}>
+                        <TableCell>{character.character.name}</TableCell>
+                        <TableCell sx={{ whiteSpace: "nowrap" }}>
+                          ({character.actor.name}{" "}
+                          {character.actor.guestStar && (
+                            <span title="Guest Star">⭐</span>
+                          )}
+                          )
+                        </TableCell>
+                        <TableCell>{character.summary}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <NotesIcon />
+              <Typography fontWeight="bold" marginLeft={1}>
+                Detailed Summary
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body2" sx={{ whiteSpace: "pre-wrap" }}>
+                {SketchData.summary}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <FormatQuoteIcon />
+              <Typography fontWeight="bold" marginLeft={1}>
+                Quotes
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                  <TableBody>
+                    {SketchData.quotes.map((quote, i) => (
+                      <TableRow key={i}>
+                        <TableCell>
+                          <Typography
+                            key={i}
+                            variant="body2"
+                            sx={{ whiteSpace: "pre-wrap" }}
+                          >
+                            {quote.text}
+                          </Typography>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4a-content"
+              id="panel4a-header"
+            >
+              <StickyNote2Icon />
+              <Typography fontWeight="bold" marginLeft={1}>
+                Notes and Trivia
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant="body2" sx={{ whiteSpace: "pre-wrap" }}>
+                {SketchData.notesAndTrivia}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+        <Box sx={{ marginTop: 2 }}>
+          <Button variant="outlined" startIcon={<ForumIcon />}>
+            Discuss on Discord
+          </Button>
+        </Box>
+      </Container>
+      {playerOpen && (
+        <Backdrop
+          onClick={handleClosePlayer}
+          open={true}
+          sx={{
+            backgroundColor: "#000d",
+            color: "white",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <iframe
+            allowFullScreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            src={SketchData.embedUrl}
+            title="YouTube video player"
+            width="800"
+            height="600"
+          ></iframe>
+        </Backdrop>
+      )}
+    </>
+  );
 }
