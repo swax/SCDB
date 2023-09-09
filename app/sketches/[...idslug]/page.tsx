929f1f838bf309cc744bbf30b943c84fae187489
swax <34883+swax@users.noreply.github.com>
2023-09-09 14:48:46 -0700

    Service layer and pulling data from database

---
+++ app/sketches/[...idslug]/page.tsx
@@ -0,0 +1,17 @@
+import { getSketch } from "@/backend/services/sketch";
+import { notFound } from "next/navigation";
+
+export default async function SketchPage({ params }: { params: { idslug: string[] } }) {
+  const [id, slug] = params.idslug;
+
+  // validate slug and redirect if invalid
+
+  const sketch = await getSketch(parseInt(id));
+
+  if (!sketch) {
+    notFound();
+  }
+
+  // Rendering
+  return <>Hello {sketch.title}</>;
+}
