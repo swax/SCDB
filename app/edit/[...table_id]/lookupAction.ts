70d29be9224ad3be4448fee0445189498f267fff
John Marshall <34883+swax@users.noreply.github.com>
2023-10-05 12:20:52 -0700

    Lookup field type added to edit service Implemented fetching, displaying and writing lookup Added new 'fixed' debounce hook Revised db schema so table names are consistent for dynamic access

--- app/edit/[...table_id]/lookupAction.ts
+++ app/edit/[...table_id]/lookupAction.ts
@@ -1,14 +1,10 @@
 "use server";
 
-const actorList = [
-  { id: 1, label: "Aidy Bryant" },
-  { id: 2, label: "Beck Bennett" },
-  { id: 3, label: "Kristen Stewart" },
-  { id: 4, label: "Kyle Mooney" },
-];
+import lookupTermsInTable from "@/backend/edit/lookupService";
+import { TableLookupField } from "@/backend/edit/tableEditConfigs";
 
-export default async function lookupAction(terms: string) {
+export default async function lookupAction(terms: string, lookupField: TableLookupField) {
   if (!terms) return [];
 
-  return actorList.filter((actor) => actor.label.toLowerCase().includes(terms));
+  return await lookupTermsInTable(terms, lookupField);
 }
---
+++ frontend/hooks/useDebounce2.ts
@@ -0,0 +1,24 @@
+import { DependencyList, useRef } from "react";
+import { useDebounce } from "react-use";
+import { UseDebounceReturn } from "react-use/lib/useDebounce";
+
+/** Fixes debounce so it doesn't run on initializaiton when the deps haven't even changed */
+export default function useDebounce2(
+  fn: Function,
+  ms?: number,
+  deps?: DependencyList
+): UseDebounceReturn {
+  const firstRun = useRef(true);
+
+  return useDebounce(
+    () => {
+      if (firstRun.current) {
+        firstRun.current = false;
+        return;
+      }
+      fn();
+    },
+    ms,
+    deps
+  );
+}
