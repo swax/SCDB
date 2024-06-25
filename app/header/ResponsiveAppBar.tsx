0829d55c341869a7724cd3aff4134d9a1af2e8df
swax <34883+swax@users.noreply.github.com>
2024-06-24 19:11:26 -0700

    Rewrote the video player component to support multiple video urls as well as reddit and facebook videos

--- app/header/ResponsiveAppBar.tsx
+++ app/header/ResponsiveAppBar.tsx
@@ -192,7 +192,7 @@ function ResponsiveAppBar() {
             display: { xs: "block", sm: "none" },
             position: "absolute",
             left: "50%",
-            transform: "translateX(-100%)",
+            transform: "translateX(-50%)",
           }}
         >
           {LogoHomeLink}
