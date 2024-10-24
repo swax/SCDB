f85aef3e36a05616117d99942a2514d8cf98085b
John Marshall <34883+swax@users.noreply.github.com>
2024-10-23 20:10:43 -0700

    Changed db env variables again Removed calling google update apis

--- app/edit/[...table_id]/actions/saveAction.ts
+++ app/edit/[...table_id]/actions/saveAction.ts
@@ -52,8 +52,11 @@ export default async function saveAction(table: TableCms, id: number) {
       const tablePath = table.name.replace("_", "-");
       const url = `https://www.sketchtv.lol/${tablePath}/${rowId}/${slugPath}`;
       const bingUpdate = sendBingUpdate(url, response);
-      const googleUpdate = sendGoogleUpdate(url, response);
-      await Promise.all([bingUpdate, googleUpdate]);
+      
+      // Disable this, not meant for how we are using it, no changes are being registered by google
+      // const googleUpdate = sendGoogleUpdate(url, response);
+
+      await Promise.all([bingUpdate]); //, googleUpdate]);
     }
 
     return response;
