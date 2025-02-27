88af9fd31c7897836b6c089acc1116ed02bd3f2c
swax <34883+swax@users.noreply.github.com>
2025-02-26 21:36:47 -0800

    Set default user role to none Added missing auth to delete api

--- app/edit/[...table_id]/actions/deleteAction.ts
+++ app/edit/[...table_id]/actions/deleteAction.ts
@@ -1,13 +1,20 @@
 "use server";
 
 import { revalidateContent } from "@/app/(content)/contentBase";
-import { catchServiceErrors, getLoggedInUser } from "@/backend/actionHelper";
-import { deleteRow, getSlugForId } from "@/backend/edit/editWriteService";
+import {
+  catchServiceErrors,
+  getLoggedInUser,
+  validateRoleAtLeast,
+} from "@/backend/actionHelper";
 import { TableCms } from "@/backend/cms/cmsTypes";
+import { deleteRow, getSlugForId } from "@/backend/edit/editWriteService";
+import { user_role_type } from "@prisma/client";
 
 export default async function deleteAction(table: TableCms, id: number) {
   return await catchServiceErrors(async () => {
     const user = await getLoggedInUser();
+
+    validateRoleAtLeast(user.role, user_role_type.Editor);
 
     const slug = await getSlugForId(table, id);
 
