b75d57be7ec72b1c9bf425a1733494f025b1638c
John Marshall <34883+swax@users.noreply.github.com>
2025-12-02 19:11:42 -0500

    A few small agent config updates

---
+++ operations/naisys/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Make executable with: chmod +x operations/naisys/start.sh
+# Change to script directory to load .env file
+cd "$(dirname "$0")"
+node ~/NAISYS/apps/naisys/dist/naisys.js program-director.yaml
