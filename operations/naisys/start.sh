f20b9a4fb1b81bffe72465c42a53e55c15e26571
swax <34883+swax@users.noreply.github.com>
2026-01-19 20:47:16 -0500

    Updated agents with new naisys ng- prefixed commands and supervisor env vars

--- operations/naisys/start.sh
+++ operations/naisys/start.sh
@@ -2,4 +2,4 @@
 # Make executable with: chmod +x operations/naisys/start.sh
 # Change to script directory to load .env file
 cd "$(dirname "$0")"
-node ~/NAISYS/apps/naisys/dist/naisys.js program-director.yaml
+node ~/NAISYS/apps/naisys/dist/naisys.js program-director.yaml --supervisor
